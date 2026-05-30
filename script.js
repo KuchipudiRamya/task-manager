0071af4723356c78a7f26a78e26e63646ecbd0e7
kuchipudi ramya <kramyasrinivas663@gmail.com>
2026-05-30 16:24:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+displayTasks();
+
+function addTask() {
+    const input = document.getElementById("taskInput");
+
+    if (input.value === "") return;
+
+    tasks.push(input.value);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    displayTasks();
+
+    input.value = "";
+}
+
+function displayTasks() {
+    const list = document.getElementById("taskList");
+
+    list.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        list.innerHTML += `
+        <li>
+            ${task}
+            <button onclick="editTask(${index})">Edit</button>
+            <button onclick="deleteTask(${index})">Delete</button>
+        </li>`;
+    });
+}
+
+function editTask(index) {
+    let newTask = prompt("Edit Task:", tasks[index]);
+
+    if (newTask) {
+        tasks[index] = newTask;
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        displayTasks();
+    }
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    displayTasks();
+}
